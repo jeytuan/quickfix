5e225b7af4bcb9e4b1b1c589b43362462c295fc7
jusnguyen <jeytech@protonmail.com>
2023-06-01 19:40:55 -0700

    Update application.go

--- application.go
+++ application.go
@@ -1,137 +1,82 @@
 package main
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "net/http"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
-    "github.com/quickfixgo/quickfix"
+	"github.com/quickfixgo/quickfix"
 )
 
 type Application interface {
-    OnCreate(sessionID quickfix.SessionID)
-    OnLogon(sessionID quickfix.SessionID)
-    OnLogout(sessionID quickfix.SessionID)
-    ToAdmin(message quickfix.Message, sessionID quickfix.SessionID)
-    FromAdmin(message quickfix.Message, sessionID quickfix.SessionID)
-    ToApp(message quickfix.Message, sessionID quickfix.SessionID)
-    FromApp(message quickfix.Message, sessionID quickfix.SessionID)
-    Fuzz(data []byte) int
+	OnCreate(sessionID quickfix.SessionID)
+	OnLogon(sessionID quickfix.SessionID)
+	OnLogout(sessionID quickfix.SessionID)
+	ToAdmin(message quickfix.Message, sessionID quickfix.SessionID)
+	FromAdmin(message quickfix.Message, sessionID quickfix.SessionID)
+	ToApp(message quickfix.Message, sessionID quickfix.SessionID)
+	FromApp(message quickfix.Message, sessionID quickfix.SessionID)
 }
 
 type MyApplication struct {
-    // Implement the Application interface
+	// Implement the Application interface
 }
 
 func (app *MyApplication) OnCreate(sessionID quickfix.SessionID) {
-    // Implement the OnCreate method
+	// Implement the OnCreate method
 }
 
 func (app *MyApplication) OnLogon(sessionID quickfix.SessionID) {
-    // Implement the OnLogon method
+	// Implement the OnLogon method
 }
 
 func (app *MyApplication) OnLogout(sessionID quickfix.SessionID) {
-    // Implement the OnLogout method
+	// Implement the OnLogout method
 }
 
 func (app *MyApplication) ToAdmin(message quickfix.Message, sessionID quickfix.SessionID) {
-    // Implement the ToAdmin method
+	// Implement the ToAdmin method
 }
 
 func (app *MyApplication) FromAdmin(message quickfix.Message, sessionID quickfix.SessionID) {
-    // Implement the FromAdmin method
+	// Implement the FromAdmin method
 }
 
 func (app *MyApplication) ToApp(message quickfix.Message, sessionID quickfix.SessionID) {
-    // Implement the ToApp method
+	// Implement the ToApp method
 }
 
 func (app *MyApplication) FromApp(message quickfix.Message, sessionID quickfix.SessionID) {
-    // Implement the FromApp method
+	// Implement the FromApp method
 }
 
-func (app *MyApplication) Fuzz(data []byte) int {
-    // Define the input data for the fuzz function
-    request := struct {
-        Username string `json:"username"`
-        Password string `json:"password"`
-    }{
-        Username: "test",
-        Password: "test",
-    }
+// Add the fuzz function as a standalone function outside of the MyApplication struct
+func Fuzz(data []byte) int {
+	// The implementation of the fuzz function goes here
+}
 
-    // Marshal the input data to JSON
-    jsonData, err := json.Marshal(request)
-    if err!= nil {
-        return 0
-    }
+func main() {
+	// Create an instance of MyApplication
+	app := &MyApplication{}
 
-    // Create a new HTTP request with the fuzzed JSON data
-    req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonData))
-    if err!= nil {
-        return 0
-    }
+	// Create a new FIX engine
+	cfg := quickfix.NewSettings()
+	storeFactory := quickfix.NewMemoryStoreFactory()
+	appFactory := quickfix.NewDefaultMessageFactory()
+	initiator := quickfix.NewInitiator(app, storeFactory, cfg, appFactory)
 
-    // Set the headers for the HTTP request
-    req.Header.Set("Content-Type", "application/json")
+	// Start the FIX engine
+	err := initiator.Start()
+	if err != nil {
+		fmt.Println("Error starting FIX engine:", err)
+		return
+	}
 
-    // Create a new HTTP client
-    client := &http.Client{}
+	// Run the fuzzing function
+	Fuzz(nil)
 
-    // Send the HTTP request and get the response
-    resp, err := client.Do(req)
-    if err!= nil {
-        return 0
-    }
-    defer resp.Body.Close()
-
-    // Check if the response status code is OK
-    if resp.StatusCode!= http.StatusOK {
-        return 0
-    }
-
-    // Read the response body
-    body, err := ioutil.ReadAll(resp.Body)
-    if err!= nil {
-        return 0
-    }
-
-    // Unmarshal the response body to a struct
-    var response struct {
-        Token string `json:"token"`
-    }
-    err = json.Unmarshal(body, &response)
-    if err!= nil {
-        return 0
-    }
-
-    // Check if the token is valid
-    if len(response.Token)!= 32 {
-        return 0
-    }
-
-    // Define the expected output data for the fuzz function
-    expected := struct {
-        Code int    `json:"code"`
-        Msg  string `json:"msg"`
-    }{
-        Code: 200,
-        Msg:  "success",
-    }
-
-    // Marshal the expected output data to JSON
-    expectedJSON, err := json.Marshal(expected)
-    if err!= nil {
-        return 0
-    }
-
-    // Compare the expected output data with the actual output data
-    if!bytes.Equal(body, expectedJSON) {
-        return 0
-    }
-
-    // Return a success status code
-    return 1
+	// Stop the FIX engine
+	initiator.Stop()
 }
